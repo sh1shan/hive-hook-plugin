415782a48003acdfd65bc2b0fa31ed78a16ba4d1
chenchaolin <578089476@qq.com>
2023-07-01 17:41:41 +0800

    1

--- src/main/java/org/data/meta/hive/util/JsonUtils.java
+++ src/main/java/org/data/meta/hive/util/JsonUtils.java
@@ -4,6 +4,7 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
 public class JsonUtils {
+    //使用这种方式避免json转换过程中导致 等号、双引号、单引号被错误翻译成其他乱码
     private static final Gson GSON = new GsonBuilder().disableHtmlEscaping().create();
 
     public JsonUtils() {
